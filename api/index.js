2e62dbe643739f2648c87ae37c52be29a4fad758
Shashank Bisht <105352161+Shashank-bisht@users.noreply.github.com>
2023-12-28 18:55:57 +0530

    done with signup

--- api/index.js
+++ api/index.js
@@ -2,16 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 dotenv.config()
 const app = express();
-
+app.use(express.json());
 mongoose.connect(process.env.MONGO).then(()=>{
-    console.log("connecte+d")
+    console.log("connected")
 }).catch((err)=>{
 console.log(err)
 })
 
 app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
 
 app.listen(8080,()=>{
     console.log("listen")
